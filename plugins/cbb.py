c8eec5f554769c32e03cce572fab72f8605c6cb3
nithe23 <animeartic39@gmail.com>
2024-11-16 15:03:58 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>🤖 ᴍʏ ɴᴀᴍᴇ :</b> <a href='http://t.me/Ruby_Hoshino_XBot'>ᴀʀᴛɪᴄ ғɪʟᴇ sᴛᴏʀᴇ</a> \n<b>📝 Language :</b> <a href='https://python.org'>Python 3</a> \n<b>📚 Library :</b> <a href='https://pyrogram.org'>Pyrogram {__version__}</a> \n<b>🚀 Server :</b> <a href='https://heroku.com'>Heroku</a> \n<b>📢 ᴄʜᴀɴɴᴇʟ :</b> <a href='https://t.me/Anime_Artic'>ᴀɴɪᴍᴇ ᴡᴇᴇᴋᴇɴᴅs</a> \n<b>🧑‍💻 ᴅᴇᴠᴇʟᴏᴘᴇʀ :</b> <a href='tg://user?id={OWNER_ID}'>ᴛʜɪs ᴘᴇʀsᴏɴ</a>",
+            text = f"<b>🤖 ᴍʏ ɴᴀᴍᴇ :</b> <a href='http://t.me/Ruby_Hoshino_XBot'>ᴀʀᴛɪᴄ ғɪʟᴇ sᴛᴏʀᴇ</a> \n<b>📝 Language :</b> <a href='https://python.org'>Python 3</a> \n<b>📚 Library :</b> <a href='https://pyrogram.org'>Pyrogram {__version__}</a> \n<b>🚀 Server :</b> <a href='https://heroku.com'>Heroku</a> \n<b>📢 ᴄʜᴀɴɴᴇʟ :</b> <a href='https://t.me/Anime_Artic'>ᴀɴɪᴍᴇ ᴀʀᴛɪᴄ</a> \n<b>🧑‍💻 ᴅᴇᴠᴇʟᴏᴘᴇʀ :</b> <a href='https://t.me/JeffreySama'>ᴛʜɪs ᴘᴇʀsᴏɴ</a>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
